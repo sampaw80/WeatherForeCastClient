d47353670029346f96304cf1e427f33deb4d9e0d
DESKTOP-IHJHGGH\Mihipal <mihipal@outlook.com>
2022-03-05 11:38:03 +0000

    Checking CI Client app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { AccountService } from './_services/account.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'The Dating app';
+  title = 'The WeatherForeCast app';
   users: any;
 
   constructor(private accountService: AccountService) {}
